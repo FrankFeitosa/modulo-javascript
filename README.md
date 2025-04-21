5a7164129d81c30aebf8c2da966be99f5a640604
FrankFeitosa <feitosfrank22@gmail.com>
2025-04-21 18:58:29 -0300

    Projeto Js Terminado

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Aqui é o repositório para aprender a linguagem de programação JavaScript
 # inicia o node
 npm init -y
 
-# abre um pacote para simular uma API
+# abre um pacote para simular uma API serve para quando a parte de FrontEnd estiver pronta para rodar o projeto enquanto aguarda o banco de dados ficar disponivel.
 npm install json-server@0.17.4
 
 # cria arquivo .json
--- projetoJs/index.js
+++ projetoJs/index.js
@@ -1,9 +1,101 @@
-function novaTarefa() {
+function botaoTarefa() {
     overlay.classList.add("active");
     criarTarefa.classList.add("active");
 }
 
-function fecharModal(){
+function fecharModal() {
     overlay.classList.remove("active");
     criarTarefa.classList.remove("active");
+}
+
+function buscarTarefas(){
+    fetch("http://localhost:3000/tarefas")
+        .then(res => res.json())
+        .then(res => {
+            inserirTarefas(res)
+        })
+} 
+buscarTarefas()
+
+function inserirTarefas(listaDeTarefas) {
+    if (listaDeTarefas.length > 0) {
+        lista.innerHTML = ""
+        listaDeTarefas.map(tarefa => {
+            lista.innerHTML += `
+                <li>
+                    <h5>${tarefa.titulo}</h5>
+                    <p>${tarefa.descricao}</p>
+                    <div class="acao">
+                        <box-icon name='trash' size="sm" onclick="deletarTarefa(${tarefa.id})"></box-icon>
+                    </div>
+                </li>
+            `
+        })
+    }
+}
+
+function novaTarefa() {
+    event.preventDefault();
+    let tarefa = {
+        titulo: titulo.value,
+        descricao: descricao.value
+    }
+    fetch("http://localhost:3000/tarefas", {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(tarefa)
+    })
+    .then(res => res.json())
+    .then(res => {
+        fecharModal();
+        buscarTarefas();
+        let form = document.querySelectorAll("#criarTarefa form");
+        form.reset();
+    })
+}
+
+function deletarTarefa(id){
+    fetch(`http://localhost:3000/tarefas/${id}`, {
+        method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(res => {
+        alert("Tarefa deletada com sucesso");
+        buscarTarefas();
+    })
+}
+
+// function pesquisarTarefas(){
+//     let li = document.querySelectorAll("ul li")
+//     if(busca.value.length > 0){
+//         li.forEach(lis => {
+//             if(!lis.children[0].innerText.includes(busca.value)){
+//                 lis.classList.add("oculto")
+//             }else {
+//                 lis.classList.remove("oculto")
+//             }
+//         })
+//     }else {
+//         li.forEach(lis =>{
+//             lis.classList.remove("oculto")
+//         })
+//     }
+// }
+
+function pesquisarTarefas(){
+    let li = document.querySelectorAll("ul li")
+    if(busca.value.length > 0){
+        li.forEach(lis => {
+            let descricao = lis.querySelector("p").innerText;
+            if(!descricao.includes(busca.value)){
+                lis.classList.add("oculto")
+            }else {
+                lis.classList.remove("oculto")
+            }
+        })
+    }else {
+        li.forEach(lis =>{
+            lis.classList.remove("oculto")
+        })
+    }
 }
